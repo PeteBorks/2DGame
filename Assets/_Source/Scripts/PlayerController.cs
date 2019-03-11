dcf8d1a47896b0e46dd7ef7201db3217fb98058a
Pedro Borges <pedroalonsoborges@hotmail.com>
2019-03-10 21:13:10 -0300

    Add SwitchDirection to PlayerController

--- Assets/_Source/Scripts/PlayerController.cs
+++ Assets/_Source/Scripts/PlayerController.cs
@@ -1,6 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿/**
+ * PlayerController.cs
+ * Created by: Pedro Borges
+ * Created on: 04/03/19 (dd/mm/yy)
+ */
+
+using Cinemachine;
 using UnityEngine;
+
 [SelectionBase]
 public class PlayerController : MonoBehaviour
 {
@@ -9,11 +15,18 @@ public class PlayerController : MonoBehaviour
     float speed = 1;
     [SerializeField]
     LayerMask groundFilter;
+    [SerializeField]
+    GameObject rightCam;
+    [SerializeField]
+    GameObject leftCam;
+    [SerializeField]
+    SpriteRenderer sprite;
     [SerializeField, Range (0,1)]
     float jumpThreshold = 0.1f;
     [SerializeField, Range(5, 10)]
     float jumpForce = 5f;
 
+    bool isFacingRight = true;
     bool isGrounded;
     bool jump;
     RaycastHit2D[] results = new RaycastHit2D[1];
@@ -38,11 +51,19 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-
         isGrounded = collider2d.Raycast(Vector2.down, filter, results, collider2d.bounds.extents.y + jumpThreshold) == 1;
         movement = new Vector2(Input.GetAxis("Horizontal") * speed, rb2d.velocity.y);
+
+        
+
         if (Input.GetButtonDown("Jump") && isGrounded)
             jump = true;
+    }
+
+    void OnRenderObject()
+    {
+        if ((movement.x < 0 && isFacingRight) || (movement.x > 0 && !isFacingRight))
+            SwitchDirection();
     }
 
     void FixedUpdate()
@@ -60,5 +81,23 @@ public class PlayerController : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawLine(transform.position, transform.position + Vector3.down * jumpThreshold);
+    }
+
+    void SwitchDirection()
+    {
+        if (isFacingRight)
+        {
+            leftCam.SetActive(true);
+            rightCam.SetActive(false);
+            
+        }
+        else
+        {
+            leftCam.SetActive(false);
+            rightCam.SetActive(true);
+        }
+        sprite.transform.localScale = new Vector3(-sprite.transform.localScale.x, sprite.transform.localScale.y, sprite.transform.localScale.z);
+        sprite.flipX = !sprite.flipX;
+        isFacingRight = !isFacingRight;
     }
 }
